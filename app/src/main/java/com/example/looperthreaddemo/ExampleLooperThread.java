cb1b16acc5e2eca0371299d5b4ac9ca3d9ab41de
KEERTHINAIK R <keerthinaik.r@outlook.com>
2022-03-02 23:46:24 +0530

    Added code for passing work as runnable to handler

---
+++ app/src/main/java/com/example/looperthreaddemo/ExampleLooperThread.java
@@ -0,0 +1,24 @@
+package com.example.looperthreaddemo;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+public class ExampleLooperThread extends Thread {
+
+    private static final String TAG = "ExampleLooperThread";
+
+    public Handler looperThreadHandler;
+
+    public Looper looper;
+
+    @Override
+    public void run() {
+        Log.d(TAG, "start of run: ");
+        Looper.prepare();
+        looper = Looper.myLooper();
+        looperThreadHandler =  new Handler();
+        Looper.loop();
+        Log.d(TAG, "end of run: ");
+    }
+}
--- app/src/main/java/com/example/looperthreaddemo/MainActivity.java
+++ app/src/main/java/com/example/looperthreaddemo/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.looperthreaddemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ExampleLooperThread exampleLooperThread = new ExampleLooperThread();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startThread(View view) {
+        exampleLooperThread.start();
+    }
+
+    public void stopThread(View view) {
+        exampleLooperThread.looper.quit();
+
+        // alternate way
+        // exampleLooperThread.looperThreadHandler.getLooper().quit();
+    }
+
+    public void taskA(View view) {
+        exampleLooperThread.looperThreadHandler.post(() -> {
+            for (int i = 0; i < 5; i++) {
+                SystemClock.sleep(1000);
+                Log.v(TAG, "taskA run: " + i);
+            }
+        });
+    }
+
+    public void taskB(View view) {
+        exampleLooperThread.looperThreadHandler.post(() -> {
+            for (int i = 0; i < 5; i++) {
+                SystemClock.sleep(1000);
+                Log.v(TAG, "taskB run: " + i);
+            }
+        });
     }
 }
